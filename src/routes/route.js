e50a52b8b60d6461d8377343ee072fd9456ded08
Jitendrayadav7999 <jitendra7999yadav@outlook.com>
2022-08-09 21:51:40 +0530

    problem 1

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,74 +3,20 @@ const express = require('express');
 
 const router = express.Router();
 
-
-router.get("/movies", function(req, res){
-     const movies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
-     res.send(movies)
-
-})
-
-
-
-router.get("/movies/:indexNumber", function(req, res){
-    const movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
-    console.log(req.params.indexNumber)
-    let movieIndex = req.params.indexNumber
-    
-    if(movieIndex<0 || movieIndex>=movies.length) {
-        
-        return res.send('The index value is not correct, Please check the it')
+ router.get("/sol1", function (req, res) {
+    //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+    let arr= [1,2,3,5,6,7]
+    let sum = 0;
+    for (var i in arr) {
+        sum = sum + arr[i];
     }
-
-   
-    let requiredMovie = movies[movieIndex]
-    res.send(requiredMovie)
-})
-
-
-router.get("/films", function(req, res){
-    const films = [ {
-        "id": 1,
-        "name": "The Shining"
-       }, {
-        "id": 2,
-        "name": "Incendies"
-       }, {
-        "id": 3,
-        "name": "Rang de Basanti"
-       }, {
-        "id": 4,
-        "name": "Finding Nemo"
-       }]
-      res.send(films) 
-})
-
-router.get("/films/:filmId", function(req, res){
-    const films = [ {
-        "id": 1,
-        "name": "The Shining"
-       }, {
-        "id": 2,
-        "name": "Incendies"
-       }, {
-        "id": 3,
-        "name": "Rang de Basanti"
-       }, {
-        "id": 4,
-        "name": "Finding Nemo"
-       }]
-
-       let filmId = req.params.filmId
-
-       for(let i = 0; i < films.length; i++){
-           let film = films[i]
-           if(film.id == filmId) {
-              
-               return res.send(film)
-           }
-       }
-       res.send("The film id doesn't match any movie")
-})
-
+ 
+    let lastDigit= arr.pop()
+    let tolalSum= lastDigit * (lastDigit+1) / 2
+    let missingNumber= tolalSum - sum
+ 
+    res.send(  { data: missingNumber  }  );
+  });
+ 
 module.exports = router;
 // adding this comment for no reason
