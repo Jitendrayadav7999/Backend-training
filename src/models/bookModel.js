0edf25182ace77a22135f1f99489efd33673c204
Jitendrayadav7999 <110589302+Jitendrayadav7999@users.noreply.github.com>
2022-08-17 00:18:49 +0530

    Update bookModel.js

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,31 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose")
 
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
-    prices: {
-        indianPrice: String,
-        europePrice: String,
+const bookSchema = new mongoose.Schema({
+    bookName: {
+        type: String,
+        require: String
     },
-    sales: {type: Number, default: 10}
+    price: {
+        indian: String,
+        european: String
+    },
+    year: {
+        type: Number,
+        default: 2021
+    },
+    tags: [],
+    authorName: String,
+    totalPages: String,
+    stockAvailable: Boolean
+
 }, { timestamps: true });
 
-
-module.exports = mongoose.model('Book', bookSchema) //users
-
-//Validation:
-//require:true
-//unique
-// default
-
-//String
-//Number
-//Date
-//Boolean
-// Arrays
-// Object
-// ObjectId
-// Buffer - not cover
+module.exports = mongoose.model("Book", bookSchema)
