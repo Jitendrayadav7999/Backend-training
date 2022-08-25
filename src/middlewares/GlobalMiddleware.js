5da971577334e89b92d4e845cf769014d83dd039
Jitendrayadav7999 <110589302+Jitendrayadav7999@users.noreply.github.com>
2022-08-25 13:03:48 +0530

    Update GlobalMiddleware.js

--- src/middlewares/GlobalMiddleware.js
+++ src/middlewares/GlobalMiddleware.js
@@ -1,5 +1,7 @@
+const moment = require("moment")
+
 const GlobalMiddleware = function (req, res, next) {
-    let timestamp = new Date()
+    let timestamp = moment().format('YYYY MM DD, h:mm:ss a')
     let ip = req.socket.remoteAddress
     let route = req.url
     console.log (timestamp,ip,route);
