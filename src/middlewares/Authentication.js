da70b61a46e1daa36d9104800ed9f3e3dd2f1056
Jitendrayadav7999 <jitendra7999yadav@outlook.com>
2022-08-29 20:10:11 +0530

    update

--- src/middlewares/Authentication.js
+++ src/middlewares/Authentication.js
@@ -6,7 +6,7 @@ const tokenverify = async function (req, res, next) {
     if (!token)
         return res.send({ status: false, msg: "token must be present" });
 
-    let decodedToken = jwt.verify(token, "jit-860-yad-799-end-211-av-9759-ra-7009-371",)
+    let decodedToken = jwt.verify(token, "jit-860-yad-799-end-211-av-9759-ra-7009-371")
     if (!decodedToken) return res.send({ status: false, message: "invalid token" });
 
     req.userId = decodedToken.userId  //Set an attribute in request object 
--- src/middlewares/Authorisation.js
+++ src/middlewares/Authorisation.js
@@ -1,14 +1,8 @@
-const jwt = require("jsonwebtoken");
-const userModel = require("../models/userModel");
-
 const checkUser = async function(req,res,next){
 //Authorisation
 let verfiyuserID = req.userId
 let userId = req.params.userId;
 if (verfiyuserID !== userId) return res.send({ status: false, msg: "User logged is not allowed to modify the requested users data" })
-
-req.userId = userId   //Set an attribute in request object 
-
 
 next()
 }
