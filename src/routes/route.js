ca128a69f3550309dfd1bcd98373a4f5e6302e9c
Jitendrayadav7999 <jitendra7999yadav@outlook.com>
2022-09-02 00:04:21 +0530

    update

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,6 +6,6 @@ router.get("/cowin/states", CowinController.getStates)
 router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
 
 // WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
-router.get("/cowin/getByDistrict", CowinController.getByDistrict)
+router.get("/cowin/getByDistrictid", CowinController.getByDistrict)
 
-.exports = router;
+module.exports = router;
