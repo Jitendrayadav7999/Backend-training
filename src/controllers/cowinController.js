f754657ec374298443dba7b5c226e531b465839c
Jitendrayadav7999 <jitendra7999yadav@outlook.com>
2022-09-01 18:06:14 +0530

    Assignment Solution

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -38,48 +38,26 @@ let getDistricts = async function (req, res) {
     }
 }
 
-let getByPin = async function (req, res) {
+  
+const getByDistrict = async function (req, res) {
     try {
-        let pin = req.query.pincode
+        let District = req.query["district_id"]
         let date = req.query.date
-        console.log(`query params are: ${pin} ${date}`)
+
         var options = {
             method: "get",
-            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pin}&date=${date}`
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${District}&date=${date}`
         }
         let result = await axios(options)
-        console.log(result.data)
         res.status(200).send({ msg: result.data })
-    }
-    catch (err) {
-        console.log(err)
-        res.status(500).send({ msg: err.message })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({ msg: error.message })
     }
 }
-
-let getOtp = async function (req, res) {
-    try {
-        let blahhh = req.body
-        
-        console.log(`body is : ${blahhh} `)
-        var options = {
-            method: "post",
-            url: `https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP`,
-            data: blahhh
-        }
-
-        let result = await axios(options)
-        console.log(result.data)
-        res.status(200).send({ msg: result.data })
-    }
-    catch (err) {
-        console.log(err)
-        res.status(500).send({ msg: err.message })
-    }
-}
-
 
 module.exports.getStates = getStates
 module.exports.getDistricts = getDistricts
 module.exports.getByPin = getByPin
 module.exports.getOtp = getOtp
+module.exports.getByDistrict = getByDistrict
